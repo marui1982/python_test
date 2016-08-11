83647ed2adc87b6418097f695092136c060bd63d
marui1982 <27438748@qq.com>
2016-08-11 12:01:35 +0800

    类继承

---
+++ test_class.py
@@ -0,0 +1,23 @@
+import sys,string
+class A1:
+    def a1(self):
+        print 'use A1 a1'
+    def a3(self):
+        print 'user A1 a3'
+
+class A2(A1):
+    def a1(self):
+        print 'use A2 a1'
+    def a2(self):
+        print 'user A2 a2'
+
+a=A1()
+a.a1()
+a.a3()
+
+print '==============='
+b=A2()
+b.a1()
+b.a2()
+b.a3()
+
